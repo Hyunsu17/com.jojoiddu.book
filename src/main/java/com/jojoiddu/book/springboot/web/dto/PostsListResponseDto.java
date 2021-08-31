3ee9a4267e7387a550c1995187459e86add6f569
Hyunsu Kim <limhgotosuccess@naver.com>
2021-08-31 17:19:43 +0900

    page150 구동확인

---
+++ src/main/java/com/jojoiddu/book/springboot/web/dto/PostsListResponseDto.java
@@ -0,0 +1,21 @@
+package com.jojoiddu.book.springboot.web.dto;
+
+import com.jojoiddu.book.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity){
+        this.id=entity.getId();
+        this.title=entity.getTitle();
+        this.author=entity.getAuthor();
+        this.modifiedDate=entity.getModifiedTime();
+    }
+}
