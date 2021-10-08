82c8c5acd48e99767801fcf5faffa59f914cc23a
Hyunsu Kim <limhgotosuccess@naver.com>
2021-10-08 11:02:07 +0900

    완성본 5차

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -2,18 +2,18 @@
 
 #쉬고 있는 profile 찾기: real1사용 중이면 real2가 쉬고있는 방식
 
-function find_idle_profie() {
+function find_idle_profile() {
 
     RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 
     if [ ${RESPONSE_CODE} -ge 400 ] #400보다 크면
     then
-      CURRENT_PROFILE = real2
+      CURRENT_PROFILE=real2
     else
       CURRENT_PROFILE=$(curl -s http://localhost/profile)
     fi
 
-    if [ ${CURRENT_PROFILE} == real1]
+    if [ ${CURRENT_PROFILE} == real1 ]
     then
       IDLE_PROFILE=real2
     else
@@ -27,7 +27,7 @@ function find_idle_profie() {
 #쉬고있는 profile port찾기
 function find_idle_port() {
 
-  IDLE_PROFILE=$(find_idle_profie)
+  IDLE_PROFILE=$(find_idle_profile)
 
   if [ ${IDLE_PROFILE} == real1 ]
   then
